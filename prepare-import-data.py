b4fd7fa947e871f0e2558fc8125afbeba3afc98a
Rumy <a-saleque@dfid.gov.uk>
2020-09-28 13:31:18 +0600

    Added more functions to import relationships between organisation and activities.

--- prepare-import-data.py
+++ prepare-import-data.py
@@ -13,8 +13,15 @@ driver = GraphDatabase.driver(uri, auth=(os.getenv("neo4jUser"), os.getenv("neo4
 def create_orgs(tx, identifier, title):
     tx.run("CREATE (:Organisations {OrgIdentifier: $identifier, OrgName: $title})", identifier=identifier, title=title)
 
-def create_activities(tx, iatiIdentifier, title):
-    tx.run("CREATE (:Activities {ActivityIdentifier: $iatiIdentifier, ActivityTitle: $title})", iatiIdentifier=iatiIdentifier, title=title)
+def create_activities(tx, iatiIdentifier, title, reportingOrgTitle, reportingOrgIdentifier):
+    tx.run("CREATE (:Activities {ActivityIdentifier: $iatiIdentifier, ActivityTitle: $title, ReportingOrgTitle: $reportingOrgTitle, ReportingOrgIdentifier: $reportingOrgIdentifier})", iatiIdentifier=iatiIdentifier, title=title,  reportingOrgTitle=reportingOrgTitle, reportingOrgIdentifier=reportingOrgIdentifier)
+
+def create_parent_to_child_relation(tx, reportedIdentifier, relatedIdentifier):
+    tx.run("MATCH (reportedActivity:Activities {ActivityIdentifier: $reportedIatiIdentifier}) MATCH (relatedActivity:Activities {ActivityIdentifier: $relatedIatiIdentifier}) MERGE (reportedActivity)-[:PARENT_OF]->(relatedActivity)", reportedIdentifier=reportedIdentifier, relatedIdentifier=relatedIdentifier)
+
+def create_parti_org_relations(tx, participatingOrgRef, activityId, participatingOrgRole, participatingOrgActivityId):
+    tx.run("MATCH (organisation:Organisations {OrgIdentifier: $participatingOrgRef}) MATCH (activity:Activities {ActivityIdentifier: $activityId}) MERGE (activity)-[:DECLARES_PARTICIPATING_ORG {role:$participatingOrgRole}]->(organisation)", participatingOrgRef=participatingOrgRef, activityId=activityId, participatingOrgRole=participatingOrgRole)
+    tx.run("MATCH (activity:Activities {ActivityIdentifier: $activityId}) MATCH (participating_activity:Activities {ActivityIdentifier: $participatingOrgActivityId}) WHERE NOT $participatingOrgActivityId = $activityId MERGE (activity)-[:DECLARES_PARTICIPATING_ACTIVITY {role:$participatingOrgRole}]->(participating_activity)", activityId=activityId, participatingOrgActivityId=participatingOrgActivityId, participatingOrgRole=participatingOrgRole)
 
 def create_index(tx):
     tx.run("create index on :Activities(ActivityIdentifier)")
@@ -44,32 +51,46 @@ count = 0
 for activity in activities_from_json['response']['docs']:
     print("Processing activity: ", count)
     # Populate the main item stores
+    tempOrgTitle = activity['reporting_org_narrative'][0] if 'reporting_org_narrative' in activity.keys() else None
+    tempOrgIdentifier = activity['reporting_org_ref'] if 'reporting_org_ref' in activity.keys() else None
     reporting_organisation_dict = {
-        "organisationTitle": activity['reporting_org_narrative'][0] if 'reporting_org_narrative' in activity.keys() else None,
-        "organisationIdentifier": activity['reporting_org_ref'] if 'reporting_org_ref' in activity.keys() else None
+        "organisationTitle": tempOrgTitle,
+        "organisationIdentifier": tempOrgIdentifier
     }
+    # Run bolt query to push data to the neo4j db
+    with driver.session() as session:
+    session.write_transaction(create_orgs, tempOrgIdentifier, tempOrgTitle)
     # Add the reporting_org data to the main organisations array
     organisation_list.append(reporting_organisation_dict.copy())
 
     # Populate the activity data in activity item store
+    tempActivityTitle = activity['title_narrative'][0] if 'title_narrative' in activity.keys() else None
+    tempActivityIdentifier = activity['iati_identifier'] if 'iati_identifier' in activity.keys() else None
+    tempActivityReportingOrgTitle = activity['reporting_org_narrative'][0] if 'reporting_org_narrative' in activity.keys() else None
+    tempActivityReportingOrgIdent = activity['reporting_org_ref'] if 'reporting_org_ref' in activity.keys() else None
     activity_dict = {
-        "title": activity['title_narrative'][0] if 'title_narrative' in activity.keys() else None,
-        "iatiIdentifier": activity['iati_identifier'] if 'iati_identifier' in activity.keys() else None
+        "title": tempActivityTitle,
+        "iatiIdentifier": tempActivityIdentifier,
+        "reportingOrgTitle": tempActivityReportingOrgTitle,
+        "reportingOrgIdentifier": tempActivityReportingOrgIdent
     }
+    # Run bolt query to push data to the neo4j db
+    with driver.session() as session:
+    session.write_transaction(create_activities, tempActivityIdentifier, tempActivityTitle, tempActivityReportingOrgTitle, tempActivityReportingOrgIdent)
     # Add the activity to the activity list
     activity_list.append(activity_dict.copy())
 
     # Create relations
     # Published by relations
-    hierarchy = activity['hierarchy'] if 'hierarchy' in activity.keys(
-    ) else None
-    activity_published_by_dict = {
+    ##hierarchy = activity['hierarchy'] if 'hierarchy' in activity.keys(
+    ##) else None
+    """ activity_published_by_dict = {
         "reportingOrgIdentifier": activity['reporting_org_ref'] if 'reporting_org_ref' in activity.keys() else None,
         "activityIatiIdentifier": activity['iati_identifier'] if 'iati_identifier' in activity.keys() else None,
         "activityHierarchy": hierarchy
-    }
+    } """
     # push the data to it's relevant list
-    activity_published_by.append(activity_published_by_dict)
+    ##activity_published_by.append(activity_published_by_dict)
 
     # Possible parent activity to child activity relation
     if 'related_activity_ref' in activity.keys():
@@ -80,6 +101,9 @@ for activity in activities_from_json['response']['docs']:
                     "relatedIatiIdentifier": related_activity
                     # "relationship": activity['related_activity_type'][index]
                 }
+                # Run bolt query to push data to the neo4j db
+                with driver.session() as session:
+                    session.write_transaction(create_parent_to_child_relation, activity['iati_identifier'], related_activity)
                 parent_to_child_relation.append(
                     activity_to_activity_relation_dict)
             elif activity['related_activity_type'][index] == '1':
@@ -90,14 +114,14 @@ for activity in activities_from_json['response']['docs']:
                 }
                 child_to_parent_relation.append(
                     activity_to_activity_relation_dict)
-            elif activity['related_activity_type'][index] == '3':
+            """ elif activity['related_activity_type'][index] == '3':
                 activity_to_activity_relation_dict = {
                     "reportedIatiIdentifier": activity['iati_identifier'],
                     "relatedIatiIdentifier": related_activity
                     # "relationship": activity['related_activity_type'][index]
                 }
                 child_to_child_relation.append(
-                    activity_to_activity_relation_dict)
+                    activity_to_activity_relation_dict) """
 
     # Populate the participating organisations
     if 'participating_org' in activity.keys() and 'reporting_org_ref' in activity.keys():
@@ -111,6 +135,8 @@ for activity in activities_from_json['response']['docs']:
                 "participatingOrgActivityId": part_json['activity_id'],
                 "participatingOrgRole": part_json['role']['code']
             }
+            with driver.session() as session:
+                    session.write_transaction(create_parti_org_relations, part_json['ref'], activity['iati_identifier'], part_json['role']['code'], part_json['activity_id'])
             participating_org_of.append(participating_org_of_dict.copy())
     print("Processing of activity: ", count, "completed.")
     count = count + 1
@@ -119,11 +145,11 @@ for activity in activities_from_json['response']['docs']:
 print("Converting to data frame..")
 organisation_list_df = pd.DataFrame(organisation_list)
 activity_list_df = pd.DataFrame(activity_list)
-activity_published_by_df = pd.DataFrame(activity_published_by)
+##activity_published_by_df = pd.DataFrame(activity_published_by)
 # activity_to_activity_relation_df = pd.DataFrame(activity_to_activity_relation)
 parent_to_child_relation_df = pd.DataFrame(parent_to_child_relation)
 child_to_parent_relation_df = pd.DataFrame(child_to_parent_relation)
-child_to_child_relation_df = pd.DataFrame(child_to_child_relation)
+##child_to_child_relation_df = pd.DataFrame(child_to_child_relation)
 participating_org_of_df = pd.DataFrame(participating_org_of)
 
 print("Sorting and cleaning duplicates..")
@@ -134,15 +160,15 @@ organisation_list_df.drop_duplicates(keep="first", inplace=True)
 activity_list_df.sort_values("iatiIdentifier", inplace=True)
 activity_list_df.drop_duplicates(keep="first", inplace=True)
 
-activity_published_by_df.sort_values("activityIatiIdentifier", inplace=True)
-activity_published_by_df.drop_duplicates(keep="first", inplace=True)
+##activity_published_by_df.sort_values("activityIatiIdentifier", inplace=True)
+##activity_published_by_df.drop_duplicates(keep="first", inplace=True)
 
 parent_to_child_relation_df.sort_values("reportedIatiIdentifier", inplace=True)
 parent_to_child_relation_df.drop_duplicates(keep="first", inplace=True)
 child_to_parent_relation_df.sort_values("reportedIatiIdentifier", inplace=True)
 child_to_parent_relation_df.drop_duplicates(keep="first", inplace=True)
-child_to_child_relation_df.sort_values("reportedIatiIdentifier", inplace=True)
-child_to_child_relation_df.drop_duplicates(keep="first", inplace=True)
+##child_to_child_relation_df.sort_values("reportedIatiIdentifier", inplace=True)
+##child_to_child_relation_df.drop_duplicates(keep="first", inplace=True)
 
 participating_org_of_df.sort_values("participatingOrgRef", inplace=True)
 participating_org_of_df.drop_duplicates(keep="first", inplace=True)
@@ -152,13 +178,13 @@ print("Building the relevant CSVs..")
 organisation_list_df.to_csv(
     r'0_organisation_list.csv', index=None, header=True)
 activity_list_df.to_csv(r'0_activity_list.csv', index=None, header=True)
-activity_published_by_df.to_csv(
-    r'0_org_to_activity_relation_list.csv', index=None, header=True)
+##activity_published_by_df.to_csv(
+##    r'0_org_to_activity_relation_list.csv', index=None, header=True)
 parent_to_child_relation_df.to_csv(
     r'0_parent_to_child_relation_list.csv', index=None, header=True)
 child_to_parent_relation_df.to_csv(
     r'0_child_to_parent_relation_list.csv', index=None, header=True)
-child_to_child_relation_df.to_csv(
-    r'0_child_to_child_relation_list.csv', index=None, header=True)
+##child_to_child_relation_df.to_csv(
+##    r'0_child_to_child_relation_list.csv', index=None, header=True)
 participating_org_of_df.to_csv(
     r'0_activity_to_participating_org_relation_list.csv', index=None, header=True)
